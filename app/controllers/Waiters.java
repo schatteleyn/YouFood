9b9cb8143099fb54e43bc703c4101d5718bd6733
Matthieu Vaidie <matthieu.vaidie@gmail.com>
2012-05-21 01:12:02 -0400

    [Update] Waiter : Controller & Views

--- app/controllers/Waiters.java
+++ app/controllers/Waiters.java
@@ -1,15 +1,16 @@
 package controllers;
 
-import java.util.List;
-import models.Table;
+import models.Restaurant;
 import models.Waiter;
 import play.data.validation.Required;
 import play.mvc.Controller;
 
 public class Waiters extends Controller {
 
-    public static void index() {
-        Waiter.findAll();
+    public static void index(Long restaurant_id) {
+        //Trouver une autre solution pour rediriger directement vers restaurants.show
+        Restaurant restaurant = Restaurant.findById(restaurant_id);
+        render(restaurant);
     }
     
     public static void find(Long id){
@@ -17,29 +18,32 @@ public class Waiters extends Controller {
         render(waiter);
     }
     
-    public static void create() {
-        render();
+    public static void create(Long id) {
+        Restaurant restaurant = Restaurant.findById(id);
+        render(restaurant);
     }
     
-    public static void saveCreate(@Required String name, List<Table> tables) {
+    public static void saveCreate(@Required Long id, @Required String firstName, @Required String lastName) {
         if (validation.hasErrors()) {
             validation.keep();
             params.flash();
             flash.error("Please correct these errors !");
-            index();
+            index(id);
         }
-        Waiter waiter = new Waiter(name, tables);
+        Restaurant restaurant = Restaurant.findById(id);
+        Waiter waiter = new Waiter(firstName, lastName, restaurant, null);
         waiter.save();
-        index();
+        index(id);
     }
     
     public static void edit(Long id) {
         Waiter waiter = Waiter.findById(id);
         render(waiter);
     }
-    public static void saveEdit(@Required Long id, @Required String name) {
+    public static void saveEdit(@Required Long id, @Required String firstName, @Required String lastName) {
         Waiter waiter = Waiter.findById(id);
-        waiter.name = name;
+        waiter.firstName = firstName;
+        waiter.lastName = lastName;
         validation.valid(waiter);
         if(validation.hasErrors()) {
           // Message errors to test in views
@@ -47,12 +51,13 @@ public class Waiters extends Controller {
           flash.success("The waiter has been updated !");
           waiter.save();
         }
-        index();
+        index(waiter.restaurant.id);
     }
     
     public static void destroy(Long id) {
         Waiter waiter = Waiter.findById(id);
+        Long restaurant_id = waiter.restaurant.id;
         waiter.delete();
-        index();
+        index(restaurant_id);
     }
 }   
--- app/models/Waiter.java
+++ app/models/Waiter.java
@@ -2,18 +2,29 @@ package models;
 
 import java.util.List;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import play.db.jpa.Model;
 
 @Entity
 public class Waiter extends Model {
     
-    public String name;
+    public String firstName;
+    public String lastName;
+    
+    //password
+    //admin
+    
+    @ManyToOne
+    public Restaurant restaurant;
+        
     @OneToMany
     public List<Table> listTables;
 
-    public Waiter(String name, List<Table> listTables) {
-        this.name = name;
+    public Waiter(String firstName, String lastName, Restaurant restaurant, List<Table> listTables) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.restaurant = restaurant;
         this.listTables = listTables;
     }
     
